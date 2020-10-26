e11e34cedb6e822474e02e33d69c3602fe9a7e3a
Lotte van 't Hof <s1112052@student.hsleiden.nl>
2020-10-26 10:27:54 +0100

    second and fourth screen

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+setTimeout(function(){
+  document.getElementById("earth").style.display = "none";
+  document.getElementById("js--body").setAttribute("style", "background-color: white;");
+}, 12000);
