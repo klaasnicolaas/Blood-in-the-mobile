2f16ce9fed0f576479326addd098f69a3e8e4ce0
Klaas Schoute <klaas_schoute@hotmail.com>
2020-11-03 04:09:38 +0100

    :truck: Zet plane JS code in een apart bestand

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-setTimeout(function(){
-  document.getElementById("earth").style.display = "none";
-  document.getElementById("js--body").setAttribute("style", "background-color: white;");
-}, 12000);
---
+++ assets/js/plane.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+    // document.getElementById("earth").style.display = "none";
+    // document.getElementById("js--body").setAttribute("style", "background-color: white;");
+    location.href='/deel-2/spullen-verzamelen.html'
+}, 10000);
