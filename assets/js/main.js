30d20c4b9461867008398bb48c695134579cd2ec
Klaas Schoute <klaas_schoute@hotmail.com>
2020-11-04 01:10:37 +0100

    :sparkles: Ga opzoek naar spullen

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// HTML block elements import
+const water_bottle_info_box = document.getElementById('item_box--water_bottle');
+const lantern_info_box = document.getElementById('item_box--lantern');
+const map_info_box = document.getElementById('item_box--map');
+const money_info_box = document.getElementById('item_box--money');
+const pocket_knife_info_box = document.getElementById('item_box--pocket_knife');
+
+// Import item icons
+const water_bottle_icon = document.getElementById('js--water_bottle');
+const lantern_icon = document.getElementById('js--lantern');
+const map_icon = document.getElementById('js--map');
+const money_icon = document.getElementById('js--money');
+const pocket_knife_icon = document.getElementById('js--pocket_knife');
+
+// Info boxes
+const info_box = document.getElementById('js--textbox_info');
+
+// Background div
+const background = document.getElementById('js--background');
+
+// Backpack / Counter
+const backpack_image = document.getElementById('js--backpack');
+const backpack_counter = document.getElementById('backpack__count');
+const backpack_counter_number = document.getElementById('backpack__count_number');
+
+// Adventure guy
+const adventure_person = document.getElementById('section__adventure-guy');
+
+// Audio import
+const street_audio = new Audio('../assets/audio/street.mp3');
+const grab_audio = new Audio('../assets/audio/grab.mp3');
