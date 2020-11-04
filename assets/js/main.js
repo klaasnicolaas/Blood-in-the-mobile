e0347566fd5ab1e7a0c8920d70130deead8e944e
Lotte van 't Hof <s1112052@student.hsleiden.nl>
2020-11-04 08:58:51 +0100

    few changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,19 @@ setTimeout(function(){
   document.getElementById("earth").style.display = "none";
   document.getElementById("js--body").setAttribute("style", "background-color: white;");
 }, 12000);
+
+
+// document.getElementById('js--type--text').innerHTML = "";
+// typeText = (textToBeTyped) =>{
+//   if(textToBeTyped != ""){
+//   document.getElementById('js--type--text').innerHTML += textToBeTyped[0];
+//   textToBeTyped.splice(0,1);
+//   setTimeout(() =>{
+//     typeText(textToBeTyped);
+//   },100);
+// }
+//
+//
+// }
+//
+// typeText(Array.from("Je telefoon abonnement is bijna verlopen en daarom ben je vandaag in een telefoonwinkel, want bij een nieuw abonnement hoort ook een nieuwe smartphone. Maar is dat eigenlijk wel verstandig?"));
